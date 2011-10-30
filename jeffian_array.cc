9f62dd6b3b05dfc6056bade0b55e0348432ea2db
huangyingw@linux_programming <huangyingw@gmail.com>
2011-10-30 22:49:27 +0800

    n

--- jeffian_array.cc
+++ jeffian_array.cc
@@ -4,29 +4,48 @@
 
 using namespace std;
 
-int isNFactored(int value, int n)
+int isJeffian(int a[ ], int len)
 {
-  int result=0,count=0;
-  if (value>0)
+  int min=100000,count=0;
+  for (int i=0;i<len;i++)
   {
-    for (int i=2;i<value;i++)
+    bool isPrime=true;
+    for (int j=2;j<a[i];j++)
     {
-      if(0==value%i)
-        count++;
+      if (0==a[i]%j)
+        isPrime=false;
     }
-    if (count==n)
-      result=1;
+    if (isPrime)
+    {
+      count++;
+      if (a[i]<min)
+        min=a[i];
+    }
   }
-  return result;
+  if (min==count && count>0)
+    return 1;
+  else
+    return 0;
 }
 
 int main() 
 {
-  cout<<isNFactored(14,2)<<endl;//1,14 has two factors, 2 and 7
-  cout<<isNFactored(18,5)<<endl;//0,18 does not have 5 factors, it only has four (2, 3, 6 and 9)
-  cout<<isNFactored(25,2)<<endl;//0,25 = 5*5 but we are only interested in distinct factors so the 5 is only counted once.
-  cout<<isNFactored(36,7)<<endl;//1,36 has seven factors (2, 3, 4, 6, 9, 12, 18). Note that 6 is counted only once.
-  cout<<isNFactored(7,0)<<endl;//1,all prime numbers are 0-factored
-  cout<<isNFactored(-5,3)<<endl;//0,the value must be greated than 0
+  int a[] ={7, 4, 11, 6, 13, 11, 17, 8, 10, 13, 7} ;//1 7 is the smallest prime in the array and there are 7 primes
+  cout<<isJeffian(a,sizeof(a)/sizeof(int))<<endl;
+
+  int b[]={3,4,7,6,5};//1 3 is the smallest prime but there are 4 primes in the array
+  cout<<isJeffian(b,sizeof(b)/sizeof(int))<<endl;
+
+  int c[]={8, 4, 6, 9};//0 the array must contain at least one prime
+  cout<<isJeffian(c,sizeof(c)/sizeof(int))<<endl;
+
+  int d[]={3, 2, 2, 2};//0 3 is the smallest prime but there is only 1 prime in the array
+  cout<<isJeffian(d,sizeof(d)/sizeof(int))<<endl;
+
+  int e[]={3, 3, 3};//1 3 is the smallest prime and there are 3 primes in the array
+  cout<<isJeffian(e,sizeof(e)/sizeof(int))<<endl;
+
+  int h[]={-3, 0, 4};//0 the array must contain at least one prime.
+  cout<<isJeffian(h,sizeof(h)/sizeof(int))<<endl;
   return 0;
 }
--- makefile
+++ makefile
@@ -1,3 +1,14 @@
 OBJECTS = jeffian_array.exe
-include ../../GNU_makefile_template
 LOCFLAGS = -I../jeffian_array
+CC = g++
+CCFLAGS = -ansi -W -Wall -lstdc++
+LOCFLAGS =
+all: $(OBJECTS)
+%.o: %.cc %.cpp
+	$(CC) $(CCFLAGS) $(LOCFLAGS) -c $< -g -o $@
+%.exe: %.o
+	$(CC) $(CCFLAGS) $(LOCFLAGS) $< -o $@
+clean:
+	rm -rf *.o core *.stackdump
+clobber: clean
+	rm -rf *.exe
